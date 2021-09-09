47b7124171423dfe71a176106d9edb88c70845c7
lai <1164258202@qq.com>
2021-09-09 08:44:04 +0800

    a

--- app/src/main/java/com/example/android_learn/MainActivity.kt
+++ app/src/main/java/com/example/android_learn/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android_learn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.main_activity_btn)
+
+        btn.setOnClickListener {
+            Toast.makeText(this, "YOU cicked Button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
